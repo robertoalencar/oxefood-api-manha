d6b5de835788b838c3a20ee45e087aee4e25f9fb
Roberto Alencar <rroberto.alencar@gmail.com>
2023-09-13 11:12:39 -0300

    Enviando arquivos

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -21,5 +22,16 @@ public class ClienteService {
         cliente.setDataCriacao(LocalDate.now());
         return repository.save(cliente);
     }
+
+    public List<Cliente> findAll() {
+  
+        return repository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+
+        return repository.findById(id).get();
+    }
+
 
 }
