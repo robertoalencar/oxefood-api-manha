77406b14b533b23f68d51572dd2ecd79b719cfda
Roberto Alencar <rroberto.alencar@gmail.com>
2023-09-20 12:07:40 -0300

    Enviando arquivos da aula

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -17,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+import br.com.ifpe.oxefood.util.MensagensDocumentacaoAPI;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -26,6 +30,7 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
+    @ApiOperation(value = MensagensDocumentacaoAPI.API_CADASTRO_CLIENTE)
     @PostMapping
     public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
 
@@ -33,18 +38,28 @@ public class ClienteController {
         return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Serviço responsável por listar todos os clientes do sistema.")
     @GetMapping
     public List<Cliente> findAll() {
   
         return clienteService.findAll();
     }
 
+    @ApiOperation(value = "Serviço responsável por obter um cliente referente ao Id passado na URL.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna o cliente."),
+        @ApiResponse(code = 401, message = "Acesso não autorizado."),
+        @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso."),
+        @ApiResponse(code = 404, message = "Não foi encontrado um registro para o Id informado."),
+        @ApiResponse(code = 500, message = "Foi gerado um erro no servidor."),
+    })
     @GetMapping("/{id}")
     public Cliente findById(@PathVariable Long id) {
 
         return clienteService.findById(id);
     }
 
+    @ApiOperation(value = "Serviço responsável por alterar um cliente do sistema.")
     @PutMapping("/{id}")
     public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
 
@@ -52,6 +67,7 @@ public class ClienteController {
        return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Serviço responsável por remover um cliente do sistema.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
 
---
+++ src/main/java/br/com/ifpe/oxefood/util/MensagensDocumentacaoAPI.java
@@ -0,0 +1,7 @@
+package br.com.ifpe.oxefood.util;
+
+public class MensagensDocumentacaoAPI {
+
+    public static final String API_CADASTRO_CLIENTE = "Serviço responsável por salvar um cliente no sistema.";
+
+}
